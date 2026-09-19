5fc9998bfaac04b2737290ba0092058ee0664f1d
Nitin Kushwah <xyzcoding06@gmail.com>
2026-09-19 16:07:09 +0530

    chore: remove debug scratch files and clean up comments for final submission

--- src/lib/eventBus.js
+++ src/lib/eventBus.js
@@ -7,6 +7,7 @@ import { EventEmitter } from 'node:events';
 // - spin_started
 // - user_eliminated
 // - winner_announced
+// TODO: EventEmitter is in-memory for a single node instance; replace with Redis Pub/Sub for horizontal scaling across nodes.
 class TypedEventBus extends EventEmitter {}
 
 export const eventBus = new TypedEventBus();
